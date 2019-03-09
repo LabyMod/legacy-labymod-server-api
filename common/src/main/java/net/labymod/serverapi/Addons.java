c05ef4cef6407127fbb68f79fbf5b364d23c3755
Manuel Kollus <officialragecode@gmail.com>
2019-03-09 18:03:30 +0100

    Add the 'Addons' class

---
+++ common/src/main/java/net/labymod/serverapi/Addons.java
@@ -0,0 +1,47 @@
+package net.labymod.serverapi;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Addons {
+
+    /**
+     * Parses the addons from the INFO plugin message
+     *
+     * @param jsonObject the json object of the message
+     * @return a list containing the message's addons
+     */
+    public static List<Addon> getAddons( JsonObject jsonObject ) {
+        if ( !jsonObject.has( "addons" ) || !jsonObject.get( "addons" ).isJsonArray() )
+            return new ArrayList<>();
+
+        List<Addon> addons = new ArrayList<>();
+
+        for ( JsonElement arrayElement : jsonObject.get( "addons" ).getAsJsonArray() ) {
+            if ( !arrayElement.isJsonObject() )
+                continue;
+
+            JsonObject arrayObject = arrayElement.getAsJsonObject();
+
+            if ( !arrayObject.has( "uuid" ) || !arrayObject.get( "uuid" ).isJsonPrimitive() || !arrayObject.get( "uuid" ).getAsJsonPrimitive().isString()
+                    || !arrayObject.has( "name" ) || !arrayObject.get( "name" ).isJsonPrimitive() || !arrayObject.get( "name" ).getAsJsonPrimitive().isString() )
+                continue;
+
+            UUID uuid = null;
+
+            try {
+                uuid = UUID.fromString( arrayObject.get( "uuid" ).getAsString() );
+            } catch ( IllegalArgumentException ex ) {
+                continue;
+            }
+
+            addons.add( new Addon( uuid, arrayObject.get( "name" ).getAsString() ) );
+        }
+
+        return addons;
+    }
+}
