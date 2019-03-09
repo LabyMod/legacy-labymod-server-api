2bc87e2772ef3a2125578a32a9f4e3c819b3332c
Manuel Kollus <officialragecode@gmail.com>
2019-03-09 18:22:44 +0100

    Remove clutter code and clean the code

--- bungeecord/src/main/java/net/labymod/serverapi/bungee/listener/PluginMessageListener.java
+++ bungeecord/src/main/java/net/labymod/serverapi/bungee/listener/PluginMessageListener.java
@@ -5,7 +5,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import lombok.Getter;
 import net.labymod.serverapi.Addon;
 import net.labymod.serverapi.bungee.LabyModPlugin;
 import net.labymod.serverapi.bungee.event.LabyModPlayerJoinEvent;
@@ -17,92 +16,64 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Class created by qlow | Jan
  */
 public class PluginMessageListener implements Listener {
 
-    @Getter
-    private final static JsonParser jsonParser = new JsonParser();
+    private static final JsonParser JSON_PARSER = new JsonParser();
 
     @EventHandler
     public void onPluginMessage( PluginMessageEvent event ) {
-        if ( !(event.getSender() instanceof ProxiedPlayer) )
+        if ( !( event.getSender() instanceof ProxiedPlayer ) )
             return;
 
-        final ProxiedPlayer player = ( ProxiedPlayer ) event.getSender();
+        ProxiedPlayer player = (ProxiedPlayer) event.getSender();
 
         // The LABYMOD plugin channel is higly deprecated and shouldn't be used - we just listen to it to retrieve old labymod clients.
         if ( event.getTag().equals( "LABYMOD" ) ) {
             // Converting the byte array into a byte buffer
             ByteBuf buf = Unpooled.wrappedBuffer( event.getData() );
-
-            try {
-                // Reading the version from the buffer
-                final String version = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
-
-                // Calling the event synchronously
-                ProxyServer.getInstance().getScheduler().schedule( LabyModPlugin.getInstance(), new Runnable() {
-                    @Override
-                    public void run() {
-                        // Calling the LabyModPlayerJoinEvent
-                        ProxyServer.getInstance().getPluginManager().callEvent( new LabyModPlayerJoinEvent( player, version, false, 0, new ArrayList<Addon>() ) );
-                    }
-                }, 0L, TimeUnit.SECONDS );
-            } catch ( RuntimeException ex ) {
-                ex.printStackTrace();
-            }
+            String version = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
+            ProxyServer.getInstance().getPluginManager().callEvent( new LabyModPlayerJoinEvent( player, version, false, 0, new ArrayList<Addon>() ) );
         }
 
         if ( event.getTag().equals( "LMC" ) ) {
             // Converting the byte array into a byte buffer
             ByteBuf buf = Unpooled.wrappedBuffer( event.getData() );
 
-            try {
-                // Reading the message key
-                final String messageKey = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
-                final String messageContents = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
-                final JsonElement jsonMessage = jsonParser.parse( messageContents );
+            String messageKey = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
+            String messageContents = LabyModPlugin.getInstance().getApi().readString( buf, Short.MAX_VALUE );
+            JsonElement jsonMessage = JSON_PARSER.parse( messageContents );
 
-                // Calling the event synchronously
-                ProxyServer.getInstance().getScheduler().schedule( LabyModPlugin.getInstance(), new Runnable() {
-                    @Override
-                    public void run() {
-                        // Listening to the INFO (join) message
-                        if ( messageKey.equals( "INFO" ) && jsonMessage.isJsonObject() ) {
-                            JsonObject jsonObject = jsonMessage.getAsJsonObject();
-                            String version = jsonObject.has( "version" )
-                                    && jsonObject.get( "version" ).isJsonPrimitive()
-                                    && jsonObject.get( "version" ).getAsJsonPrimitive().isString() ? jsonObject.get( "version" ).getAsString() : "Unknown";
+            // Listening to the INFO (join) message
+            if ( messageKey.equals( "INFO" ) && jsonMessage.isJsonObject() ) {
+                JsonObject jsonObject = jsonMessage.getAsJsonObject();
+                String version = jsonObject.has( "version" )
+                        && jsonObject.get( "version" ).isJsonPrimitive()
+                        && jsonObject.get( "version" ).getAsJsonPrimitive().isString() ? jsonObject.get( "version" ).getAsString() : "Unknown";
 
-                            boolean chunkCachingEnabled = false;
-                            int chunkCachingVersion = 0;
+                boolean chunkCachingEnabled = false;
+                int chunkCachingVersion = 0;
 
-                            if ( jsonObject.has( "ccp" ) && jsonObject.get( "ccp" ).isJsonObject() ) {
-                                JsonObject chunkCachingObject = jsonObject.get( "ccp" ).getAsJsonObject();
+                if ( jsonObject.has( "ccp" ) && jsonObject.get( "ccp" ).isJsonObject() ) {
+                    JsonObject chunkCachingObject = jsonObject.get( "ccp" ).getAsJsonObject();
 
-                                if ( chunkCachingObject.has( "enabled" ) )
-                                    chunkCachingEnabled = chunkCachingObject.get( "enabled" ).getAsBoolean();
+                    if ( chunkCachingObject.has( "enabled" ) )
+                        chunkCachingEnabled = chunkCachingObject.get( "enabled" ).getAsBoolean();
 
-                                if ( chunkCachingObject.has( "version" ) )
-                                    chunkCachingVersion = chunkCachingObject.get( "version" ).getAsInt();
-                            }
+                    if ( chunkCachingObject.has( "version" ) )
+                        chunkCachingVersion = chunkCachingObject.get( "version" ).getAsInt();
+                }
 
-                            ProxyServer.getInstance().getPluginManager().callEvent( new LabyModPlayerJoinEvent( player, version,
-                                    chunkCachingEnabled, chunkCachingVersion, Addon.getAddons( jsonObject ) ) );
-                            return;
-                        }
-
-                        // Calling the LabyModPlayerJoinEvent
-                        ProxyServer.getInstance().getPluginManager().callEvent( new MessageReceiveEvent( player, messageKey, jsonMessage ) );
-                    }
-                }, 0L, TimeUnit.SECONDS );
-            } catch ( RuntimeException ex ) {
-                ex.printStackTrace();
+                ProxyServer.getInstance().getPluginManager().callEvent( new LabyModPlayerJoinEvent( player, version,
+                        chunkCachingEnabled, chunkCachingVersion, Addon.getAddons( jsonObject ) ) );
+                return;
             }
+
+            // Calling the LabyModPlayerJoinEvent
+            ProxyServer.getInstance().getPluginManager().callEvent( new MessageReceiveEvent( player, messageKey, jsonMessage ) );
         }
     }
-
 }
